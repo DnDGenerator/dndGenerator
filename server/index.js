707882526e711ec4657fa175b03b629c6567fdaf
Sfamurri <asfamurri@dxc.com>
2020-01-31 10:16:33 -0600

    not sure if cors is still needed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log('we are live');
