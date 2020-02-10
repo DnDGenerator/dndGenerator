0754cec85aeba9b180156e25ec8d55fb99001718
Sfamurri <asfamurri@dxc.com>
2020-02-10 13:20:57 -0600

    weird nress

--- app/components/lootOptions.js
+++ app/components/lootOptions.js
@@ -13,10 +13,10 @@ class LootOptions extends React.Component{
                     Select CR range
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item eventKey="0" onSelect={this.props.handleCRSelection}>0-4</Dropdown.Item>
-                    <Dropdown.Item eventKey="5" onSelect={this.props.handleCRSelection}>5-10</Dropdown.Item>
-                    <Dropdown.Item eventKey="11" onSelect={this.props.handleCRSelection}>11-16</Dropdown.Item>
-                    <Dropdown.Item eventKey="17" onSelect={this.props.handleCRSelection}>17+</Dropdown.Item>
+                    <Dropdown.Item eventKey="0-4" onSelect={this.props.handleCRSelection}>0-4</Dropdown.Item>
+                    <Dropdown.Item eventKey="5-10" onSelect={this.props.handleCRSelection}>5-10</Dropdown.Item>
+                    <Dropdown.Item eventKey="11-16" onSelect={this.props.handleCRSelection}>11-16</Dropdown.Item>
+                    <Dropdown.Item eventKey="17+" onSelect={this.props.handleCRSelection}>17+</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
         )
