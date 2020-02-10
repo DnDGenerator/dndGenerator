4a809b949f4f0723bb1d3a123a28737b5ef1c8a0
Sfamurri <asfamurri@dxc.com>
2020-02-10 13:17:07 -0600

    for got to return in render

--- app/components/lootOptions.js
+++ app/components/lootOptions.js
@@ -7,17 +7,19 @@ class LootOptions extends React.Component{
 
 
     render(){
-        <Dropdown>
-            <Dropdown.Toggle variant="primary" id="challenge-rating">
-                Select CR range
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                <Dropdown.Item eventKey="0" onSelect={this.props.handleCRSelection}>0-4</Dropdown.Item>
-                <Dropdown.Item eventKey="5" onSelect={this.props.handleCRSelection}>5-10</Dropdown.Item>
-                <Dropdown.Item eventKey="11" onSelect={this.props.handleCRSelection}>11-16</Dropdown.Item>
-                <Dropdown.Item eventKey="17" onSelect={this.props.handleCRSelection}>17+</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
+        return(
+            <Dropdown>
+                <Dropdown.Toggle variant="primary" id="challenge-rating">
+                    Select CR range
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item eventKey="0" onSelect={this.props.handleCRSelection}>0-4</Dropdown.Item>
+                    <Dropdown.Item eventKey="5" onSelect={this.props.handleCRSelection}>5-10</Dropdown.Item>
+                    <Dropdown.Item eventKey="11" onSelect={this.props.handleCRSelection}>11-16</Dropdown.Item>
+                    <Dropdown.Item eventKey="17" onSelect={this.props.handleCRSelection}>17+</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        )
     }
 }
 
