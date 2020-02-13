cc92a24aed9378ea0d710fd0ddac6e90d0deb07e
Sfamurri <asfamurri@dxc.com>
2020-02-13 11:17:28 -0600

    created a door object

--- server/doorsClass.js
+++ server/doorsClass.js
@@ -23,8 +23,29 @@ class Door{
         }else if(this.typeRoll === 18){
             this.type = 'portcullis, locked in place';
         }else if(this.typeRoll === 19){
-            
+            this,type = 'secret door';
+        }else{
+            this.type = 'secret door, barred or locked';
         }
+        if(this.beyondRoll < 3){
+            this.beyond = "Passage extending 10ft., then T intersection extending 10ft. to the right and left"
+        }else if(this.beyondRoll < 9){
+            this.beyond = "Passage 20 ft. straight ahead"
+        }else if(this.beyondRoll < 19){
+            this.beyond = "Chamber (roll on the Chamber table)"
+        }else if(this.beyondRoll === 19){
+            this.beyond = "Stairs (roll on the Stairs table)"
+        }else{
+            this.beyond = "False door with trap"
+        }
+        this.getType = this.getType.bind(this);
+        this.getBeyond = this.getBeyond.bind(this);
+    }
+    getType(){
+        return this.type;
+    }
+    getBeyond(){
+        return this.beyond;
     }
 }
 
