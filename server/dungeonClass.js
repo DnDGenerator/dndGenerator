355fcf1c39e31f3649dd3cb3fdbaf08f5b5ce5fc
Sfamurri <asfamurri@dxc.com>
2020-02-14 14:43:58 -0600

    added methods to the dungeonclass

--- server/dungeonClass.js
+++ server/dungeonClass.js
@@ -5,6 +5,7 @@ const Passage = require('./passagesClass');
 
 class Dungeon{
     constructor(mapWidth, mapHeight){
+        this.piecesOfDungeon = [];
         this.numDoors = 0;
         this.numChambers = 0;
         this.numPassages = 0;
@@ -23,5 +24,66 @@ class Dungeon{
                 this.map[i][j] = unusedSpace;
             }
         }
+        
+        this.makeADoor = this.makeADoor.bind(this);
+        this.makeAPassage = this.makeAPassage.bind(this);
+        this.makeAChamber = this.makeAChamber.bind(this);
+        this.storePieceOfDungeon = this.storePieceOfDungeon.bind(this);
+        this.getCurrentMap = this.getCurrentMap.bind(this);
+        this.updateMap = this.updateMap.bind(this);
+        this.translatedMap = this.translatedMap.bind(this);
+    }
+    makeADoor(){
+        const door = new Door();
+        this.numDoors+=1;
+        return door;
+    }
+    makeAChamber(){
+        const chamber = new Chamber();
+        this.numChambers+=1;
+        return chamber;
+    }
+    makeAPassage(origin){
+        const passage = new Passage(origin);
+        this.numPassages+=1;
+        return passage;
+    }
+    storePieceOfDungeon(piece){
+        this.piecesOfDungeon.push(piece);
+    }
+    getCurrentMap(){
+        return this.map;
+    }
+    updateMap(modifiedMap){
+        this.map = modifiedMap;
+    }
+    translatedMap(){
+        const translatedMap = [];
+        for(let i = 0; i < this.map.length; i++){
+            translatedMap[i] = [];
+            for(let j = 0; j < this.map[i].length; j++){
+                switch(this.map[i][j]){
+                    case this.unusedSpace:
+                        translatedMap[i][j] = "unusedSpace";
+                        break;
+                    case this.chamber:
+                        translatedMap[i][j] = "chamber";
+                        break;
+                    case this.door:
+                        translatedMap[i][j] = "door";
+                        break;
+                    case this.passage:
+                        translatedMap[i][j] = "passage";
+                        break;
+                    case this.stairs:
+                        translatedMap[i][j] = "stairs";
+                        break;
+                    case this.exit:
+                        translatedMap[i][j] = "exit";
+                        break;
+                }
+            }
+        }
+        return translatedMap;
     }
 }
