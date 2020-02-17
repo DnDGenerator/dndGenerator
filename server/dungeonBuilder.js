c353c4a936b0774d9de8a7e854aaa1c1dc9e05f6
Sfamurri <asfamurri@dxc.com>
2020-02-17 12:23:58 -0600

    hope this fixes

--- server/dungeonBuilder.js
+++ server/dungeonBuilder.js
@@ -2,66 +2,12 @@ const location = require('./dungeonLocation');
 const creator = require('./dungeonCreator');
 const purpose = require('./dungeonPurpose');
 const history = require('./dungeonHistory');
-const Dungeon = require('./dungeonClass');
 module.exports = ()=>{
     const dungeon = {};
     dungeon.location = location();
     dungeon.creator = creator();
     dungeon.purpose = purpose();
     dungeon.history = history();
-    const dungeonMap = new Dungeon(500,500)
     
-    
-    const startingObj = dungeonMap.getStartingInfo();
-
-    const checkCord = (x, y, lastSpace, room)=>{
-        let nextSpace = null;
-        for(let i = 0; i < room.exitLocations.length; i++){
-            if(room.exits[i].x === x && room.exits[i].y === y){
-                let exitType =room.exitType[i];
-
-                switch(exitType){
-                    case "door":
-                        nextSpace = dungeonMap.makeADoor();
-                        return {exitType, nextSpace};
-                    case "passage":
-                        nextSpace = dungeonMap.makeAPassage(lastSpace);
-                        return {exitType, nextSpace};
-                    case "chamber":
-                        nextSpace = dungeonMap.makeAChamber();
-                        return {exitType, nextSpace};
-                }
-            }
-        }
-        return {exitType:"unused", nextSpace};
-
-    }
-
-    const recursiveConstruction = (numDoorsLeft, numPassagesLeft, room)=>{
-        if(numDoorsLeft === 0 && numPassagesLeft === 0){
-            return;
-        }
-        const currentMap = dungeonMap.getCurrentMap();
-        let lastSpace = "unused";
-        for(let x = 0; x < dungeonMap.mapWidth; x++){
-            for(let y = 0; y < dungeonMap.mapHeight; y++){
-                //need to procederually plot stuff
-                currentSpace = checkCord(x,y, lastSpace, room)
-                currentMap[x][y] = currentSpace.exitType;
-                lastSpace = currentMap[x][y];
-                if(currentSpace.nextSpace !== null){
-                    room = currentMap[x][y].nextSpace;
-                }
-                
-            }
-        }
-        return recursiveConstruction(dungeonMap.getCurrentNumDoorsLeftToPlace(), dungeonMap.getCurrentNumPassagesLeftToPlace(), room);
-    }
-
-    // recursivgit eConstruction(dungeonMap.getCurrentNumDoorsLeftToPlace(), dungeonMap.getCurrentNumPassagesLeftToPlace(), startingObj)
-
-    // recursiveConstruction(dungeonMap.getCurrentNumDoorsLeftToPlace(), dungeonMap.getCurrentNumPassagesLeftToPlace());
-    // dungeon.mapObj = dungeongMap;
-
     return dungeon;
 }
