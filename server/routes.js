24f63a6933f7d38aaf4d76f1dafd83ff5cf447e4
Sfamurri <asfamurri@dxc.com>
2020-03-06 16:47:04 -0600

    1000x1000 was too much

--- server/routes.js
+++ server/routes.js
@@ -38,7 +38,7 @@ app.get('/dungeon', (req, res)=>{
 })
 
 app.get(`/map`, (req, res)=>{
-    const map = new Map(1000, 1000);
+    const map = new Map(500, 500);
     map.createMap();
     res.send({map:map.getConvertedMap()})
 })
