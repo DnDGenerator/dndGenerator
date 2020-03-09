f288e7cc98c631c1bf7e2ef05f6c016ab6fb5397
Sfamurri <asfamurri@dxc.com>
2020-03-09 15:24:36 -0500

    tweaking cell sizes

--- app/components/map.js
+++ app/components/map.js
@@ -159,8 +159,8 @@ class Map extends React.Component{
                     case "WC":
                         this.ctx.fillStyle = fillColors[16];
                 }
-                this.ctx.strokeRect(adjustedX, adjustedY, adjustedX+10, adjustedY+10)
-                this.ctx.fillRect(adjustedX, adjustedY, adjustedX+10,adjustedY+10)
+                this.ctx.strokeRect(adjustedX, adjustedY, adjustedX+20, adjustedY+20)
+                this.ctx.fillRect(adjustedX, adjustedY, adjustedX+20,adjustedY+20)
             })
         });
     }
