3e9ae17bf64833ade0d00675cdec961c072e4533
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2020-08-24 21:01:18 -0500

    added exit types and locations to startingroomv2

--- server/mapGenFolder/startingRoomV2.js
+++ server/mapGenFolder/startingRoomV2.js
@@ -3,39 +3,56 @@ class StartingRoom{
     constructor(){
         this.width = 2;
         this.length = 2;
+        this.exitLocations = [];
+        this.exitTypes = [];
 
         const startingRoll = dice.roll(`1d10`).result;
         switch(startingRoll){
             case 1:
                 this.width = 20/5;
                 this.length = 20/5;
+                this.exitLocations = ['w', 'n', 'e', 's'];
+                this.exitTypes = ['passageAnchor', 'passageAnchor', 'passageAnchor', 'passageAnchor'];
                 break;
             case 2:
                 this.width = 20/5;
                 this.length = 20/5;
+                this.exitLocations = ['w', 'n', 'e'];
+                this.exitTypes = ['doorAnchor', 'passageAnchor', 'doorAnchor'];
                 break;
             case 3:
                 this.width = 40/5;
                 this.length = 40/5;
+                this.exitLocations = ['w', 'n', 'e'];
+                this.exitTypes = ['doorAnchor', 'doorAnchor', 'doorAnchor'];
                 break;
             case 4:
                 this.width = 80/5;
                 this.length = 20/5;
+                this.exitLocations = ['w', 'n', 'e', 's'];
+                this.exitTypes = ['passageAnchor', 'doorAnchor', 'passageAnchor','doorAnchor'];
                 break;
             case 5:
                 this.width = 20/5;
                 this.length = 40/5;
+                this.exitLocations = ['w', 'n', 'e', 's'];
+                this.exitTypes = ['passageAnchor', 'passageAnchor', 'passageAnchor', 'passageAnchor']
                 break;
             case 6:
             case 7:
             case 8:
                 this.width = 20/5;
                 this.length = 20/5;
+                this.exitLocations = ['w', 'n', 'e', 's'];
+                this.exitTypes = ['doorAnchor', 'doorAnchor', 'passageAnchor', 'doorAnchor']
                 break;
             case 9:
             case 10:
+                const options = ['w', 'n', 'e', 's'];
                 this.width = 10/5;
                 this.length = 5/5;
+                this.exitLocations = [`${options[dice.roll(`1d${options.length}`).result - 1]}`];
+                this.exitTypes = ['passageAnchor'];
                 break;
         }
 
